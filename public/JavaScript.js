ec129eb26f3bc362f8b58b4f67403acd8a57654b
BoteroM89 <n.boterom@gmail.com>
2014-08-03 18:16:32 -0500

    Carpeta public

---
+++ public/JavaScript.js
@@ -0,0 +1,21 @@
+﻿//function loadSender() {
+//    $.ajax({
+//        type: "POST",
+//        url: "/hello",
+//        cache: false,
+//        dataType: "plain/text",
+//        success: function (result) {
+
+//        }
+//    });
+//}
+
+$("#boton").click(function () {
+    var bla = $("#input").val();
+    //alert(bla);
+    $.post("http://localhost:8080/hello",
+    {
+        input: bla,
+        city: "Duckburg"
+    });
+});
